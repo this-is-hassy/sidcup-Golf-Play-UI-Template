d84c3a8608496a294647936c3f9e8efe3a92faaa
this-is-hassy <126436517+this-is-hassy@users.noreply.github.com>
2024-02-14 08:52:14 +0500

    first commit

--- README.md
+++ README.md
@@ -1,36 +1,14 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Sidcup Golf Play Animated Template
 
-## Getting Started
+Welcome to the 'Sidcup Golf Play' project, an animated template crafted with Next.js, Tailwind CSS, and custom CSS animations.
 
-First, run the development server:
+## Features
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- **Animated Design:**
+  - Engaging CSS animations bring the world of golf to life, creating a dynamic and visually captivating template.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- **Next.js:**
+  - Utilizing Next.js for efficient server-side rendering and a seamless development workflow.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- **Tailwind CSS:**
+  - Incorporating Tailwind CSS for a modern and responsive design, enhanced by tailored CSS animations.
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,105 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import React from "react";
+import styles from "./dynamic.module.css";
+import { useRef, useEffect } from "react";
 
 export default function Home() {
+  const cursorRef = useRef(null);
+  const blurRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (cursorRef.current && blurRef.current) {
+        cursorRef.current.style.left = e.clientX + "px";
+        cursorRef.current.style.top = e.clientY + "px";
+        blurRef.current.style.left = e.clientX - 300 + "px";
+        blurRef.current.style.top = e.clientY - 300 + "px";
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="w-full flex justify-start items-center px-32 gap-8 text-white font-semibold text-lg fixed z-50 top-5 ">
+        <Image src="/logo.png" alt="logo" width={100} height={100}></Image>
+        <a
+          href="#"
+          className="hover:text-[#98c717] transform hover:scale-110 duration-200 ease-linear"
+        >
+          TOPTRACER RANGE
+        </a>
+        <a
+          href="#"
+          className="hover:text-[#98c717] transform hover:scale-110 duration-200 ease-linear"
+        >
+          GOLF LESSONS
+        </a>
+        <a
+          href="#"
+          className="hover:text-[#98c717] transform hover:scale-110 duration-200 ease-linear"
+        >
+          ADVENTURE GOLF
+        </a>
+        <a
+          href="#"
+          className="hover:text-[#98c717] transform hover:scale-110 duration-200 ease-linear"
+        >
+          COFFEE SHOP
+        </a>
+        <a
+          href="#"
+          className="hover:text-[#98c717] transform hover:scale-110 duration-200 ease-linear"
+        >
+          LEAGUES
+        </a>
+      </nav>
+
+      <div>
+        {/* Your cursor content */}
+        <div
+          className=" h-6 w-6 bg-[#95C11E] rounded-full fixed z-50 ease-linear duration-150"
+          ref={cursorRef}
+          id="cursor"
+        ></div>
+        {/* Your blur content */}
+        <div
+          className=" h-[300px] w-[300px] bg-[#98c717] rounded-full fixed blur-[150px] -z-30 duration-100 ease-linear  "
+          ref={blurRef}
+        ></div>
+      </div>
+
+      <video
+        autoPlay
+        loop
+        muted
+        className="-z-50 h-screen w-full fixed object-cover"
+        src={require("../../public/mainVideo.mp4")}
+      />
+      <div className="-z-40 h-screen  w-full bg-black bg-opacity-30">
+        <div className="relative text-white flex items-center justify-center flex-col w-screen h-screen z-40 text-center pt-20">
+          <div className={styles.double_head}>
+            <h1 className="relative text-9xl font-extrabold text-white before:content-[attr(before)] before:absolute before:-z-20 before:text-black before:-top-[5px] before:-left-[5px] before:text-9xl before:font-extrabold ">
+              EAT. DRINK. PLAY.
+            </h1>
+          </div>
+
+          <h2 className="font-extrabold text-3xl mt-4">
+            WELCOME TO SIDCUP FAMILY GOLF!
+          </h2>
+          <p className="text-lg font-semibold w-3/5 mt-4">
+            Sidcup Family Golf is a multipurpose golf facility located in
+            Sidcup, South East London. Passionate about technology, player
+            development and making golf fun and accessible to everyone.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
